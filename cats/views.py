6cfe57f8a9d25dc54273ddb7c0f68de0ffd27159
Sergey Morgunov <Bo-Jack-Bo@yandex.ru>
2024-10-14 22:00:21 +0400

    Шпарга

--- cats/views.py
+++ cats/views.py
@@ -1,19 +1,106 @@
-from rest_framework import status
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
+from rest_framework import viewsets
 
 from .models import Cat
 from .serializers import CatSerializer
 
 
-@api_view(['GET', 'POST'])
-def cat_list(request):
-    if request.method == 'POST':
-        serializer = CatSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    cats = Cat.objects.all()
-    serializer = CatSerializer(cats, many=True)
-    return Response(serializer.data)
+class CatViewSet(viewsets.ModelViewSet):
+    queryset = Cat.objects.all()
+    serializer_class = CatSerializer
+
+
+# class CatList(generics.ListCreateAPIView):
+#     queryset = Cat.objects.all()
+#     serializer_class = CatSerializer
+
+
+# class CatDetail(generics.RetrieveUpdateDestroyAPIView):
+#     queryset = Cat.objects.all()
+#     serializer_class = CatSerializer
+
+
+# from django.shortcuts import get_object_or_404
+# from rest_framework.decorators import api_view
+# from rest_framework.response import Response
+# from rest_framework import status
+# from .models import Post
+# from .serializers import PostSerializer
+
+
+# @api_view(['GET', 'POST'])
+# def api_posts(request):
+#     if request.method == 'POST':
+#         serializer = PostSerializer(data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data, status=status.HTTP_201_CREATED)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+#     posts = Post.objects.all()
+#     serializer = PostSerializer(posts, many=True)
+#     return Response(serializer.data)
+
+
+# @api_view(['GET', 'PUT', 'PATCH', 'DELETE'])
+# def api_posts_detail(request, pk):
+#     post = get_object_or_404(Post, id=pk)
+#     if request.method == 'PUT' or request.method == 'PATCH':
+#         serializer = PostSerializer(post, data=request.data, partial=True)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data, status=status.HTTP_200_OK)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+#     elif request.method == 'DELETE':
+#         post.delete()
+#         return Response(status=status.HTTP_204_NO_CONTENT)
+#     serializer = PostSerializer(post)
+#     return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+# from django.shortcuts import get_object_or_404
+# from rest_framework.views import APIView
+# from rest_framework.response import Response
+# from rest_framework import status
+# from .models import Post
+# from .serializers import PostSerializer
+
+
+# class APIPost(APIView):
+#     def get(self, request):
+#         posts = Post.objects.all()
+#         serializer = PostSerializer(posts, many=True)
+#         return Response(serializer.data)
+
+#     def post(self, request):
+#         serializer = PostSerializer(data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data, status=status.HTTP_201_CREATED)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+# class APIPostDetail(APIView):
+#     def get(self, request, pk):
+#         post = get_object_or_404(Post, id=pk)
+#         serializer = PostSerializer(post)
+#         return Response(serializer.data, status=status.HTTP_200_OK)
+
+#     def put(self, request, pk):
+#         post = get_object_or_404(Post, id=pk)
+#         serializer = PostSerializer(post, data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data, status=status.HTTP_200_OK)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+#     def patch(self, request, pk):
+#         post = get_object_or_404(Post, id=pk)
+#         serializer = PostSerializer(post, data=request.data, partial=True)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data, status=status.HTTP_200_OK)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+#     def delete(self, request, pk):
+#         post = get_object_or_404(Post, id=pk)
+#         post.delete()
+#         return Response(status=status.HTTP_204_NO_CONTENT)
--- kittygram/urls.py
+++ kittygram/urls.py
@@ -1,9 +1,20 @@
-from django.urls import path
+from rest_framework.routers import DefaultRouter
 
-from cats.views import cat_list
+from django.urls import include, path
+
+from cats.views import CatViewSet
+
+# Создаётся роутер
+router = DefaultRouter()
+# Вызываем метод .register с нужными параметрами
+router.register('cats', CatViewSet)
+# В роутере можно зарегистрировать любое количество пар "URL, viewset":
+# например
+# router.register('owners', OwnerViewSet)
+# Но нам это пока не нужно
 
 urlpatterns = [
-   path('cats/', cat_list),
+    # Все зарегистрированные в router пути доступны в router.urls
+    # Включим их в головной urls.py
+    path('', include(router.urls)),
 ]
-
-
